937d3a34e7de86160e940dcb9b1a6e0dda3a7ce1
algoritmus <attila.desy@gmail.com>
2019-04-30 20:19:27 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,4 +6,9 @@ mkdir /root/pipeline/jenkins_home
 mkdir /root/pipeline/portainer
 mkdir /root/pipeline/portainer/data
 mv browsers.json /root/pipeline/selenoid/config/
+
+ip=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2| cut -d' ' -f1)
+ipdash=$(echo $ip | sed 's/\./-/g')
+SELENOIDIP="ip$ipdash-$SESSION_ID-4444.direct.$PWD_HOST_FQDN"
+export SELENOIDIP
 docker pull selenoid/vnc:chrome_74.0
